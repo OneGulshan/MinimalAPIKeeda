b4a1e5d3ef51a9aaff4c29ae0f5e1010df470113
Gulshan <gulshankumar.mailid01@gmail.com>
2023-04-20 18:27:26 +0530

    Add project files.

---
+++ MinimalAPIKeeda/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPIKeeda.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsPopular { get; set; }
+    }
+    class ProductDb : DbContext
+    {
+        public ProductDb(DbContextOptions<ProductDb> options) : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
---
+++ MinimalAPIKeeda/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPIKeeda.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProductDb>(options => options.UseInMemoryDatabase("ProductList"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapPost("/productItems",
+    async (Product product, ProductDb _context) =>
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return Results.Created($"/productItems/{product.Id}", product);//on this product id our result is returned.
+    });
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
